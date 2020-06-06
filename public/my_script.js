103be6a62b078f2fcd5cb632c2bb2d68bb2e0382
Gor <7495500@gmail.com>
2020-06-06 22:37:22 +0300

    Pass id to add_to_cart

--- public/my_script.js
+++ public/my_script.js
@@ -1,6 +1,10 @@
-function something () {
+const something = () => {
   let x = window.localStorage.getItem('item_2')
   x++
   window.localStorage.setItem('item_2', x)
   alert(x)
+}
+
+const add_to_cart = (id) => {
+  alert('Pizza with id' + id)
 }
