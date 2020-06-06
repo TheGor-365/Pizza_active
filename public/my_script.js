07947fb72a2db7e60fc3bd7d8739282abcdaa7ec
Gor <7495500@gmail.com>
2020-06-07 02:21:12 +0300

    Add cart counter

--- public/my_script.js
+++ public/my_script.js
@@ -6,5 +6,22 @@ const something = () => {
 }
 
 const add_to_cart = (id) => {
-  alert('Pizza with id' + id)
+  let key = 'product_' + id
+  let x = window.localStorage.getItem(key)
+  x++
+  window.localStorage.setItem(key, x)
+  // show count of items in cart
+  alert('Items in your cart ' + cart_get_number_of_items())
+}
+
+const cart_get_number_of_items = () => {
+  let cnt = 0
+  for (let i = 0; i < window.localStorage.length; i++) {
+    let key = window.localStorage.key(i) // get key, hh[x] = hh[(x += 1)]
+    let value = window.localStorage.getItem(key) // get value, in ruby: y = hh[x]
+    if (key.indexOf('product_') === 0) {
+      cnt += value * 1
+    }
+  }
+  return cnt
 }
