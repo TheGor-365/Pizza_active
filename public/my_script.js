51af5d77b8eaf3a6103a87a3c644dea67e43a003
Gor <7495500@gmail.com>
2020-06-08 16:14:59 +0300

    Add total price

--- public/my_script.js
+++ public/my_script.js
@@ -1,56 +1,48 @@
-const something = () => {
-  let x = window.localStorage.getItem('item_2')
-  x++
-  window.localStorage.setItem('item_2', x)
-  alert(x);
-}
-
-
 const add_to_cart = (id) => {
   let key = 'product_' + id;
-  let x = window.localStorage.getItem(key)
-  x++
-  window.localStorage.setItem(key, x)
+  let x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
 
-  update_orders_input()
-  update_orders_button()
+  update_orders_input();
+  update_orders_button();
 }
 
 
 const update_orders_input = () => {
-  let orders = cart_get_orders()
-  $('#orders_input').val(orders)
+  let orders = cart_get_orders();
+  $('#orders_input').val(orders);
 }
 
 
 const update_orders_button = () => {
-  let text = 'Cart (' + cart_get_number_of_items() + ')'
-  $('#orders_button').val(text)
+  let text = 'Cart (' + cart_get_number_of_items() + ')';
+  $('#orders_button').val(text);
 }
 
 
 const cart_get_number_of_items = () => {
   let cnt = 0
   for (let i = 0; i < window.localStorage.length; i++) {
-    let key = window.localStorage.key(i) // get key, hh[x] = hh[(x += 1)]
-    let value = window.localStorage.getItem(key) // get value, in ruby: y = hh[x]
+    let key = window.localStorage.key(i); // get key, hh[x] = hh[(x += 1)]
+    let value = window.localStorage.getItem(key); // get value, in ruby: y = hh[x]
     if (key.indexOf('product_') === 0) {
-      cnt += value * 1
+      cnt = cnt + value * 1;
     }
   }
-  return cnt
+  return cnt;
 }
 
 
 const cart_get_orders = () => {
-  let orders = ''
+  let orders = '';
   for (let i = 0; i < window.localStorage.length; i++) {
-    let key = window.localStorage.key(i)
-    let value = window.localStorage.getItem(key)
+    let key = window.localStorage.key(i);
+    let value = window.localStorage.getItem(key);
 
-    if (key.indexOf('product_') === 0) {
-      orders = orders + key + '=' + value + ','
+    if (key.indexOf('product_') == 0) {
+      orders = orders + key + '=' + value + ',';
     }
   }
-  return orders
+  return orders;
 }
