56a5ac1b82a2ead42f9d05b8c174e3fa21f0c8ef
Gor <7495500@gmail.com>
2020-06-08 20:39:23 +0300

    Replace cart content on cancel

--- public/my_script.js
+++ public/my_script.js
@@ -46,3 +46,14 @@ const cart_get_orders = () => {
   }
   return orders;
 }
+
+const cancel_order = () => {
+  window.localStorage.clear()
+
+  update_orders_input()
+  update_orders_button()
+
+  $('#cart').text('Your cart is now empty')
+
+  return false
+}
